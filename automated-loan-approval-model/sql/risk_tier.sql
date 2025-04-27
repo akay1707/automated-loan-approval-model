148d30bd39999d3e9bd293ecd562c63731e089d4
Akshat <akshat.sharma8008@gmail.com>
2025-04-27 22:42:12 +0530

    Delete automated-loan-approval-model directory

--- automated-loan-approval-model/sql/risk_tier.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    applicant_id,
-    CASE 
-        WHEN (credit_score >= 750 AND dti_ratio <= 0.35 AND ltv_ratio <= 0.80) THEN 'Low Risk'
-        WHEN (credit_score BETWEEN 650 AND 749 AND dti_ratio BETWEEN 0.36 AND 0.50) THEN 'Medium Risk'
-        ELSE 'High Risk'
-    END AS risk_tier
-FROM loan_applications;
